6d7f94e1c6a92e9e672002e259efb71887f318ec
julilc <julietalopezceratto@gmail.com>
2024-11-24 21:12:50 -0300

    Hago cambios en .rqst

--- rqst.py
+++ rqst.py
@@ -5,19 +5,7 @@ import matplotlib.pyplot as plt
 from ipywidgets import interact, IntSlider
 from PIL import Image 
 
-def binarize(img: np.array, tr: int = 150, maxv: int = 255) -> np.array:
-    '''
-    Esta función recibe una imagen y la binariza según vlaores de tr y maxv recibidos.
-    img: imagen en escala de grises
-    tr : thresh.
-    maxv: máximo valor de imagen de salida.
-    '''
-    _, img_bin = cv2.threshold(img, tr, maxv, cv2.THRESH_BINARY_INV)
-    return img_bin
-
-
-
-# Defininimos función para mostrar imágenes
+# Función para mostrar imágenes
 def imshow(img, new_fig=True, title=None, color_img=False, blocking=False, colorbar=True, ticks=False):
     if new_fig:
         plt.figure()
